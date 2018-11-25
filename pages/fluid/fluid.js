8e5bfc384f688e8d4d8ca33ae7286db2840f03d5
Ellen Lo <lowing@bu.edu>
2018-11-25 10:07:47 +0100

    fluid + motion page grid ok

--- pages/fluid/fluid.js
+++ pages/fluid/fluid.js
@@ -1,6 +1,7 @@
 var canvas;
 var grid_size;
 var maxVerticalLines;
+var numHorizontalLines = 36;
 var bgMode;
 var strokeMode;
 var bgChanged;
@@ -19,10 +20,19 @@ function setup() {
     maxVerticalLines = 15;
   }
   grid_size = windowWidth / maxVerticalLines;
-  h = grid_size * 36;
+  h = grid_size * numHorizontalLines;
   canvas = createCanvas(windowWidth, h);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
+  $(function() {
+    var $body = $(document);
+    $body.bind('scroll', function() {
+      // "Disable" the horizontal scroll.
+      if ($body.scrollLeft() !== 0) {
+        $body.scrollLeft(0);
+      }
+    });
+  });
   $('.title-motion').bind('mouseover', onHoverMotion).bind('mouseout', onLeaveMotion);
   bgMode = 255;
   strokeMode = 0;
@@ -37,46 +47,44 @@ function draw() {
   stroke(strokeMode);
   for(var x = grid_size; x < windowWidth; x+=grid_size) {
     if( responsiveMode == 1 ) {
-      // if (x >= grid_size && x <= grid_size * 3) {
-      //   var numHorizontalLines = Math.floor(h / grid_size);
-      //   var blockTop = (numHorizontalLines - 1) * grid_size;
-      //   line(x, 0, x, blockTop);
-      //   line(x, blockTop + grid_size, x, h);
-      // } else
-      if (x > grid_size * 9 && x < grid_size * 14) {
-        var blockTop = grid_size;
-        line(x, 0, x, blockTop);
-        line(x, blockTop + 5 * grid_size, x, h);
+      if (x > grid_size * 9 + 1 && x < grid_size * 14 - 1) {
+        line(x, 0, x, grid_size);
+        if(x > grid_size * 11 - 1 && x < grid_size * 14 - 1) line(x, 14.5 * grid_size, x, grid_size * 15);
+        else line( x, 14 * grid_size, x, 15* grid_size);
+        line(x, grid_size * 33, x, h);
+      } else if (x >= grid_size && x <= grid_size * 3) {
+        line(x, 0, x, h - grid_size);
       } else {
         line(x, 0, x, h);
       }
     }
-    else if (responsiveMode == 0) {
-      if (x >= grid_size && x <= grid_size * 2) {
-        var numHorizontalLines = Math.floor(h / grid_size);
-        var blockTop = (numHorizontalLines - 1) * grid_size;
-        line(x, 0, x, blockTop);
-        line(x, blockTop + 2 * grid_size, x, h);
-      } else {
-        line(x, 0, x, h);
-      }
-    }
+    // else if (responsiveMode == 0) {
+    //   if (x >= grid_size && x <= grid_size * 2) {
+    //     var numHorizontalLines = Math.floor(h / grid_size);
+    //     var blockTop = (numHorizontalLines - 1) * grid_size;
+    //     line(x, 0, x, blockTop);
+    //     line(x, blockTop + 2 * grid_size, x, h);
+    //   } else {
+    //     line(x, 0, x, h);
+    //   }
+    // }
   }
   for(var y = grid_size; y < h; y+=grid_size) {
     if(responsiveMode == 1) {
-      if( y > grid_size && y <= grid_size * 5) {
+      if( (y > grid_size && y <= grid_size * 5) || (y >= grid_size * 19 + 1 && y <= grid_size * 21 - 1) || (y >= grid_size * 25 + 1 && y <= grid_size * 27 - 1) || (y >= grid_size * 31 + 1 && y <= grid_size * 33 - 1) ) {
         line(0, y, grid_size * 9, y);
         line(grid_size * 14, y, windowWidth, y);
       } else {
         line(0, y, windowWidth, y);
       }
-    } else if (responsiveMode == 0) {
-      if( y >= h - grid_size ) {
-        line(grid_size * 3 , y, windowWidth, y);
-      } else {
-        line(0, y, windowWidth, y);
-      }
     }
+    // else if (responsiveMode == 0) {
+    //   if( y >= h - grid_size ) {
+    //     line(grid_size * 3 , y, windowWidth, y);
+    //   } else {
+    //     line(0, y, windowWidth, y);
+    //   }
+    // }
   }
 }
 
@@ -87,18 +95,20 @@ function onHoverMotion() {
                             'color': '#000',
                             '-webkit-text-stroke': ' #FFF 0.5px'
                           } );
-  $('.text').css('color', "#FFF");
+  $('.text').css('color', '#FFF');
+  $('.title-fluid').css('color', '#FFF');
   $('#large-logo').attr('src', '../../assets/img/large-light-logo.png');
-  $('.white-space').css('background-color', '#000');
+  // $('.white-space').css('background-color', '#000');
 }
 
 function onLeaveMotion() {
   bgMode = 255;
   strokeMode = 0;
   $('.title-motion').css('-webkit-text-stroke', '0px');
-  $('.text').css('color', "#000");
+  $('.text').css('color', '#000');
+  $('.title-fluid').css('color', '#000');
   $('#large-logo').attr('src', '../../assets/img/large-dark-logo.png');
-  $('.white-space').css('background-color', '#FFF');
+  // $('.white-space').css('background-color', '#FFF');
 }
 
 function windowResized() {
@@ -110,6 +120,6 @@ function windowResized() {
     maxVerticalLines = 15;
   }
   grid_size = windowWidth / maxVerticalLines;
-  h = grid_size * 36;
+  h = grid_size * numHorizontalLines;
   resizeCanvas(windowWidth, h);
 }
