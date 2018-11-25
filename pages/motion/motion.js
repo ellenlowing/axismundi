2c660364d571d5a940c049a1642750afdf2ebcfc
Ellen Lo <lowing@bu.edu>
2018-11-25 11:11:43 +0100

    add glitch on hover to motion page

--- pages/motion/motion.js
+++ pages/motion/motion.js
@@ -11,6 +11,11 @@ var responsiveMode;
 var minWidth = 480;
 var h = 1024;
 
+// glitch variables
+var mouseMovedCounter, mouseMovedFlag, maxMouseMovedCounter;
+var xOffset, yOffset, maxOffset, offset;
+var prevTop, prevBottom, prevLeft, prevRight;
+
 function setup() {
   if( windowWidth <= minWidth ) {
     responsiveMode = 0;
@@ -37,6 +42,15 @@ function setup() {
   bgMode = 0;
   strokeMode = 255;
   bgChanged = false;
+  mouseMovedFlag = 0;
+  mouseMovedCounter = 0;
+  maxMouseMovedCounter = 25; // number of glitch ticks
+  maxOffset = 1.2;
+  offset = maxOffset;
+  prevTop = 0;
+  prevBottom = 0;
+  prevLeft = 0;
+  prevRight = 0;
   strokeWeight(0.5);
   background(bgMode);
   stroke(strokeMode);
@@ -45,6 +59,30 @@ function setup() {
 function draw() {
   background(bgMode);
   stroke(strokeMode);
+
+  // get neighboring 4 lines based on mouse location
+  var top = grid_size * Math.floor(mouseY / grid_size); // 0
+  var bottom = top + grid_size + 3; // 1
+  var left = grid_size * Math.floor(mouseX / grid_size); // 2
+  var right = left + grid_size + 3; // 3
+
+  // check if neighboring lines change
+  if(top === prevTop && bottom == prevBottom && left == prevLeft && right == prevRight) {
+    // turn on glitch if neighboring lines dont change (mouse stays)
+    if(mouseMovedFlag == 0) mouseMovedFlag = 1;
+    if(mouseMovedFlag == 1) {
+      if(mouseMovedCounter < maxMouseMovedCounter) {
+        mouseMovedCounter++;
+        offset = map(mouseMovedCounter, 0, maxMouseMovedCounter, maxOffset, 0);
+      }
+    }
+  } else {
+    // reset all if neighboring lines change
+    mouseMovedFlag = 0;
+    mouseMovedCounter = 0;
+    offset = maxOffset;
+  }
+
   for(var x = grid_size; x < windowWidth; x+=grid_size) {
     if( responsiveMode == 1 ) {
       if (x > grid_size * 9 + 1 && x < grid_size * 14 - 1) {
@@ -58,16 +96,6 @@ function draw() {
         line(x, 0, x, h);
       }
     }
-    // else if (responsiveMode == 0) {
-    //   if (x >= grid_size && x <= grid_size * 2) {
-    //     var numHorizontalLines = Math.floor(h / grid_size);
-    //     var blockTop = (numHorizontalLines - 1) * grid_size;
-    //     line(x, 0, x, blockTop);
-    //     line(x, blockTop + 2 * grid_size, x, h);
-    //   } else {
-    //     line(x, 0, x, h);
-    //   }
-    // }
   }
   for(var y = grid_size; y < h; y+=grid_size) {
     if(responsiveMode == 1) {
@@ -78,13 +106,47 @@ function draw() {
         line(0, y, windowWidth, y);
       }
     }
-    // else if (responsiveMode == 0) {
-    //   if( y >= h - grid_size ) {
-    //     line(grid_size * 3 , y, windowWidth, y);
-    //   } else {
-    //     line(0, y, windowWidth, y);
-    //   }
-    // }
+  }
+
+  if(mouseMovedFlag == 1 && mouseMovedCounter < maxMouseMovedCounter) {
+    // avoid blank spaces
+    if( (left >= grid_size * 9 && right <= grid_size * 15 && top >= grid_size && bottom <= grid_size * (numHorizontalLines - 2)) ||
+        (left >= 0 && right <= grid_size * 5 && top >= (numHorizontalLines-1) * grid_size) )
+      return;
+
+    // clear mouse area on canvas
+    fill(bgMode);
+    stroke(bgMode);
+    rect(left-1, top-1, grid_size+2, grid_size+2);
+
+    // draw glitch
+    stroke(255);
+    xOffset = 0;
+    yOffset = 0;
+    for(var x = left; x <= right; x += grid_size) {
+      for(var y = top; y < bottom; y ++) {
+        if(int(random(0, 10)) == 1) {
+          xOffset += random(-offset, offset);
+        }
+        point(x + xOffset, y);
+      }
+    }
+    for(var y = top; y <= bottom; y+= grid_size) {
+      for(var x = left; x < right; x++) {
+        if(int(random(0, 10)) == 1) {
+          yOffset += random(-offset, offset);
+        }
+        point(x, y + yOffset);
+      }
+    }
+  }
+
+  // store previous neighboring lines if change
+  if(mouseMovedFlag == 0){
+    prevTop = top;
+    prevBottom = bottom;
+    prevLeft = left;
+    prevRight = right;
   }
 }
 
