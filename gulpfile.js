23aba2bbfb2a587f25133ee2bf7cc8e0cb7ec8ef
Ellen Lo <lowing@bu.edu>
2019-02-02 00:29:27 -0500

    add gulp taskrunner

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglifyes = require('uglify-es');
+const composer = require('gulp-uglify/composer');
+const uglify = composer(uglifyes, console);
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+const merge = require('merge-stream');
+
+gulp.task('sass', function() {
+  return gulp.src('assets/scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('minify', function() {
+  const motion = gulp.src('pages/motion/motion.js')
+    .pipe(uglify())
+    .pipe(rename( (path) => {
+        path.basename += '.min';
+    }))
+    .pipe(gulp.dest('pages/motion'));
+
+  const fluid = gulp.src('pages/fluid/fluid.js')
+    .pipe(uglify())
+    .pipe(rename( (path) => {
+        path.basename += '.min';
+    }))
+    .pipe(gulp.dest('pages/fluid'));
+
+  const home = gulp.src('./home.js')
+    .pipe(uglify())
+    .pipe(rename( (path) => {
+        path.basename += '.min';
+    }))
+    .pipe(gulp.dest('./'));
+
+  const css = gulp.src('assets/css/style.css')
+    .pipe(cssnano())
+    .pipe(rename( (path) => {
+      path.basename += '.min';
+    }))
+    .pipe(gulp.dest('assets/css'));
+
+  let streams = merge(motion, fluid);
+  streams.add(home);
+  streams.add(css);
+  return streams;
+});
+
+gulp.task('default', gulp.series('sass', 'minify'));
